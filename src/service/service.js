07a614f33225913721d7d72a3fc9b6add6a33622
karthickselvendran <selvendrankarthick@gmail.com>
2022-03-11 02:59:50 +0530

    first commit

--- src/service/service.js
+++ src/service/service.js
@@ -1,2 +1,2 @@
-// export const API = "https://mern-todo-app-karthick.herokuapp.com"
-export const API = "http://localhost:8000"
+export const API = "https://mern-todo-app-karthick.herokuapp.com"
+// export const API = "http://localhost:8000"
